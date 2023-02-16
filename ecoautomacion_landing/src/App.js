b842659e8bd0b5d21482250ba0effb93aec1bb5f
TomasSanchez-FrontEndDev <tomasgonzalosanchez1@gmail.com>
2023-02-16 10:43:18 -0300

    nt important

--- ecoautomacion_landing/src/App.js
+++ ecoautomacion_landing/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PreNavBar from './Components/PreNavbar';
 import Navbar from './Components/NavBar';
 import PreHero from './Components/PreHero';
-import Hero from './Components/Hero'
+import Hero from './Components/Hero';
 
 const App = () => {
   return (
@@ -13,6 +13,6 @@ const App = () => {
       <Hero />
     </div>
   );
-}
+};
 
 export default App;
