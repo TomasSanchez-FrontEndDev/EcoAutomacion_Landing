81bc4214f57925776518910bfe0cb98e9781fdf8
TomasSanchez-FrontEndDev <tomasgonzalosanchez1@gmail.com>
2023-02-16 10:45:32 -0300

    Changes

---
+++ ecoautomacion_landing/src/Components/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div className='flex'>
+      <div>
+        <h1>Productos</h1>
+        <a href="">Especiales</a>
+        <a href="">Estandar</a>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Products;
